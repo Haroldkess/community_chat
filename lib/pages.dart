0b2c803ad0684cf163077d8b9469c825074a0538
Haroldkess <haroldkess77@gmail.com>
2019-11-20 13:37:27 +0100

    first commit

---
+++ lib/pages.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Page extends StatefulWidget {
+  @override
+  _PageState createState() => _PageState();
+}
+
+class _PageState extends State<Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("my app"),
+      ),
+    );
+  }
+}
